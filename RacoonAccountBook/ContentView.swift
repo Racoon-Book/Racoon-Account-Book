3b889b3150ea5a413b2f0332b2f36caa3650ff83
yxj <564197835@qq.com>
2021-07-14 20:21:52 +0800

    Initial Commit

---
+++ RacoonAccountBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RacoonAccountBook
+//
+//  Created by 杨希杰 on 2021/7/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RacoonAccountBook/RacoonAccountBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RacoonAccountBookApp.swift
+//  RacoonAccountBook
+//
+//  Created by 杨希杰 on 2021/7/14.
+//
+
+import SwiftUI
+
+@main
+struct RacoonAccountBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
