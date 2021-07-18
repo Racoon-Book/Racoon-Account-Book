59c7d04e77c3b09a0462e25795c841b5d4344f5a
yxj <564197835@qq.com>
2021-07-18 20:35:11 +0800

    prototype IncomeExpenditureView

--- RacoonAccountBook/View/Main/BookTab/IncomeExpenditureView.swift
+++ RacoonAccountBook/View/Main/BookTab/IncomeExpenditureView.swift
@@ -2,12 +2,47 @@ import SwiftUI
 
 struct IncomeExpenditureView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("收支")
+            HStack {
+                // TODO: 这里要用geometryreader
+                WeeklyView(isWeek: true, expenditure: 130.5, income: 0)
+                MonthlyView(isMonth: true, expenditure: 650.5, income: 0)
+            }
+        }
     }
 }
 
-struct IncomeExpenditureView_Previews: PreviewProvider {
-    static var previews: some View {
-        IncomeExpenditureView()
+struct WeeklyView: View {
+    var isWeek: Bool = true
+    var expenditure: Float
+    var income: Float
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(isWeek ? "本周支出" : "7日支出")
+            Text(String(format: "%.1f", expenditure))
+            Text(isWeek ? "本周收入\(String(format: "%.1f", income))元" : "7日收入\(String(format: "%.1f", income))元")
+        }
     }
 }
+
+struct MonthlyView: View {
+    var isMonth: Bool = true
+    var expenditure: Float
+    var income: Float
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(isMonth ? "本月支出" : "30日支出")
+            Text(String(format: "%.1f", expenditure))
+            Text(isMonth ? "本月收入\(String(format: "%.1f", income))元" : "30日收入\(String(format: "%.1f", income))元")
+        }
+    }
+}
+
+// struct IncomeExpenditureView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        IncomeExpenditureView()
+//    }
+// }
