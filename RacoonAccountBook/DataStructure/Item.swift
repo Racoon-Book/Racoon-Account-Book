c587ecd88e9781ea02fc8cad4de468eeb926dbb5
yxj <564197835@qq.com>
2021-08-14 00:06:19 +0800

    refactor: add comments in Item.swift

--- RacoonAccountBook/DataStructure/Item.swift
+++ RacoonAccountBook/DataStructure/Item.swift
@@ -1,30 +1,34 @@
 import Foundation
 import SwiftDate
 
-// [Item] - 基础数据结构加上软件管理用的其他数据为一个完整的Item
+/// MetaItem加上软件管理所使用的其他数据
 struct Item: Identifiable, Hashable {
-    // [Identifiable]
+    // Identifiable
     var id: Int
 
-    // [生成Item时自动生成]
-    let createdAt = DateInRegion(region: regionChina) // 创建时自动生成日期
-    var updatedAt = DateInRegion(region: regionChina) // 创建时自动生成日期 只在创建时自动生成
-
-    // [Core Data]
-    var metadata: MetaItem
-
-    // [Hashable]
+    // Hashable
     static func == (lhs: Item, rhs: Item) -> Bool {
         return lhs.id == rhs.id
     }
 
-    // [Hashable]
     // https://stackoverflow.com/a/56401466/14298786 虽然看不懂
+    // Hashable
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
     }
+
+    // MARK: - 生成Item时自动生成
+
+    /// 创建时自动生成日期
+    let createdAt = DateInRegion(region: regionChina)
+    /// 创建时自动生成日期 只在创建时自动生成
+    var updatedAt = DateInRegion(region: regionChina)
+
+    /// 一条账目的核心内容
+    var metadata: MetaItem
 }
 
+// 将Item转换为字符串
 extension Item: CustomStringConvertible {
     var description: String {
         let createdAt: String = self.createdAt.toFormat("yyyy/M/d", locale: Locales.chineseChina)
