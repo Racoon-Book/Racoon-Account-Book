8a032602d2e6ebefbad3fa2c8caa8bda4d26ac2e
c0per <60544162+c0per@users.noreply.github.com>
2021-08-21 21:34:21 +0800

    add init() to App

--- RacoonAccountBook/App/RacoonAccountBookApp.swift
+++ RacoonAccountBook/App/RacoonAccountBookApp.swift
@@ -8,6 +8,17 @@ struct RacoonAccountBookApp: App {
     @StateObject var RacoonSheetConfig = SheetConfigModel()
 
     let persistenceController = PersistenceController.shared
+    
+    init() {
+        #if DEV
+        // 在 DEV 下，当数据库为空时，插入 Testdata
+        if (Expense.all(context: persistenceController.container.viewContext).first == nil) {
+            for metadata in testMetaItems {
+                Expense.create(context: persistenceController.container.viewContext, metadata: metadata)
+            }
+        }
+        #endif
+    }
 
     var body: some Scene {
         WindowGroup {
