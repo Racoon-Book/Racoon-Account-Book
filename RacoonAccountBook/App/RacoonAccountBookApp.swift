420f59707c296493d46122125905616f3b101012
c0per <60544162+c0per@users.noreply.github.com>
2021-08-21 19:41:43 +0800

    add NSManagedObjectContext to App.

--- RacoonAccountBook/App/RacoonAccountBookApp.swift
+++ RacoonAccountBook/App/RacoonAccountBookApp.swift
@@ -7,11 +7,14 @@ struct RacoonAccountBookApp: App {
     @StateObject var RacoonAccountBook = AccountBookModel()
     @StateObject var RacoonSheetConfig = SheetConfigModel()
 
+    let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
             MainView()
                 .environmentObject(RacoonAccountBook)
                 .environmentObject(RacoonSheetConfig)
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
