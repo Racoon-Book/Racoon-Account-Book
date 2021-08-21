9a1b1fe343d706ce6838782e176d359bda467fef
yxj <564197835@qq.com>
2021-08-21 20:23:49 +0800

    fix: update originalText reflects createdAt

--- RacoonAccountBook/App/MetaItem/TextField/OriginalTextField.swift
+++ RacoonAccountBook/App/MetaItem/TextField/OriginalTextField.swift
@@ -2,6 +2,8 @@ import SwiftDate
 import SwiftUI
 
 struct OriginalTextField: View {
+    @EnvironmentObject var RacoonSheetConfig: SheetConfigModel
+
     var hint: String = ""
 
     @Binding var isEditing: Bool
@@ -31,8 +33,13 @@ struct OriginalTextField: View {
     }
 
     private func UpdateMetaItem() {
+        // 是编辑模式则创建日期不变
+        let spentMoneyAt: DateInRegion = RacoonSheetConfig.shared.isEditMode ?
+            metadata_inputting.spentMoneyAt :
+            OriginalText2SpentMoneyAt(from: metadata_inputting.originalText ?? "") ?? DateInRegion(region: regionChina)
+
         metadata_inputting.update(
-            spentMoneyAt: OriginalText2SpentMoneyAt(from: metadata_inputting.originalText ?? "") ?? DateInRegion(region: regionChina),
+            spentMoneyAt: spentMoneyAt,
             event: OriginalText2Event(from: metadata_inputting.originalText ?? "") ?? "",
             amount_float: OriginalText2Amount(from: metadata_inputting.originalText ?? "") ?? 0.0,
             generatedTags: OriginalText2GeneratedTags(from: metadata_inputting.originalText ?? ""))
