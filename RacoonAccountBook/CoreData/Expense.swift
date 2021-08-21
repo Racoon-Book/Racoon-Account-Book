c9df437709d58150d7e295fec13e2ae4ac721357
c0per <60544162+c0per@users.noreply.github.com>
2021-08-21 23:47:17 +0800

    add Entity Tag, add properties to Expense, Story

--- RacoonAccountBook/CoreData/Expense.swift
+++ RacoonAccountBook/CoreData/Expense.swift
@@ -5,16 +5,22 @@ extension Expense {
     
     // MARK: Access
     
-    static func all(context: NSManagedObjectContext) -> [Expense] {
+    static func all(context: NSManagedObjectContext, story_only: Bool = false) -> [Expense] {
         let request = NSFetchRequest<Expense>(entityName: "Expense")
+        if (story_only) {
+            request.predicate = NSPredicate(format: "story != nil")
+        }
         return (try? context.fetch(request)) ?? []
     }
     
-    static func last_month(context: NSManagedObjectContext) -> [Expense] {
+    static func last_month(context: NSManagedObjectContext, story_only: Bool = false) -> [Expense] {
         let request = NSFetchRequest<Expense>(entityName: "Expense")
         
         // TODO: Date
-        request.predicate = NSPredicate(format: "spentAt > %@", Date() - 1.months as NSDate)
+        request.predicate = NSPredicate(format: "spentAt_ > %@", Date() - 1.months as NSDate)
+        if (story_only) {
+            request.predicate = NSPredicate(format: "story != nil")
+        }
         return (try? context.fetch(request)) ?? []
     }
     
@@ -25,9 +31,9 @@ extension Expense {
         
         expense.originalText = metadata.originalText
         // FIXME: convert DateInRegion to Date
-        expense.spentAt = Date()
+        expense.spentAt = metadata.spentMoneyAt
         expense.event = metadata.event
-        expense.amount_float = metadata.amount_float
+        expense.amount = metadata.amount_float
         
         if (metadata.story != nil) {
             expense.story = Story.create(context: context, story: metadata.story!)
@@ -35,6 +41,27 @@ extension Expense {
         
         expense.objectWillChange.send()
         try? context.save()
-        printLog("add expense")
+    }
+    
+    // Properties
+    
+    var createdAt: DateInRegion {
+        get { createdAt_?.convertTo(region: regionChina) ?? DateInRegion("1970-01-01 00:00:00", region: regionChina)! }
+        set { createdAt_ = newValue.date }
+    }
+    
+    var updatedAt: DateInRegion {
+        get { updatedAt_?.convertTo(region: regionChina) ?? DateInRegion("1970-01-01 00:00:00", region: regionChina)! }
+        set { updatedAt_ = newValue.date }
+    }
+    
+    var spentAt: DateInRegion {
+        get { spentAt_?.convertTo(region: regionChina) ?? DateInRegion("1970-01-01 00:00:00", region: regionChina)! }
+        set { spentAt_ = newValue.date }
+    }
+    
+    var event: String {
+        get { event_ ?? "暂无事件" }
+        set { event_ = newValue }
     }
 }
--- RacoonAccountBook/CoreData/Story.swift
+++ RacoonAccountBook/CoreData/Story.swift
@@ -9,7 +9,7 @@ extension Story {
     static func create(context: NSManagedObjectContext, story story_data: MetaItem.Story) -> Story {
         let story = Story(context: context)
         
-        story.rating = Int16(story_data.rating!)
+        story.rating = story_data.rating!
         story.emoji = story_data.emoji
         story.text = story_data.text
         
@@ -17,5 +17,12 @@ extension Story {
         try? context.save()
         
         return story
+    }
+    
+    // MARK: Properties
+    
+    var rating: Int {
+        get { Int(rating_) }
+        set { rating_ = Int16(newValue) }
     }
 }
