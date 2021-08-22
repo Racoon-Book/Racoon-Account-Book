cec6a7ac3c54bd4d70411bc97ae02c0f6ac6efdd
c0per <60544162+c0per@users.noreply.github.com>
2021-08-22 11:01:41 +0800

    change order in StoryTab

--- RacoonAccountBook/App/StoryTab/StoryTab.swift
+++ RacoonAccountBook/App/StoryTab/StoryTab.swift
@@ -10,25 +10,17 @@ struct StoryTab: View {
         // TODO: 像账本那样做一个深色的统计页面应该比较好
         if expenses.count != 0 {
             NavigationView {
-                VStack {
-                    ScrollViewReader { scrollView in
-                        ScrollView(.vertical) {
-                            VStack {
-                                ForEach(expenses, id: \.self) { expense in
-                                    ItemStoryView(expense: expense)
-                                }
-                                .padding(.horizontal, 10) // 让圆角矩形边框不靠边
-                            }
-                            .onAppear {
-                                // 出现的时候滑到最下面
-                                scrollView.scrollTo(expenses.last?.id) // FIXME: 这个没效果不知道为什么 感觉好像和账本页面的ScrollView冲突了 可能排个序就好了
-                            }
-                        }.padding([.vertical], 5) // 让上下两个stroy不靠边
+                ScrollView(.vertical) {
+                    VStack {
+                        ForEach(expenses, id: \.self) { expense in
+                            ItemStoryView(expense: expense)
+                        }
+                        .padding(.horizontal, 10) // 让圆角矩形边框不靠边
                     }
                 }
+                .padding(.vertical, 10) // 让上下两个stroy不靠边
                 .navigationTitle("最近的财记")
                 .navigationBarTitleDisplayMode(.inline)
-                .edgesIgnoringSafeArea(.top)
             }
 
         } else {
