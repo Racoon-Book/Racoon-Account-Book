616c226784d0579f54bc181f4d86022cb576ef9e
c0per <60544162+c0per@users.noreply.github.com>
2021-08-22 11:01:28 +0800

    add days stuff to Expense

--- RacoonAccountBook/CoreData/Expense.swift
+++ RacoonAccountBook/CoreData/Expense.swift
@@ -6,6 +6,7 @@ extension Expense {
     
     static func all(context: NSManagedObjectContext, story_only: Bool = false) -> [Expense] {
         let request = NSFetchRequest<Expense>(entityName: "Expense")
+        request.sortDescriptors = [NSSortDescriptor(key: "spentAt_", ascending: false)]
         if story_only {
             request.predicate = NSPredicate(format: "story != nil")
         }
@@ -14,6 +15,7 @@ extension Expense {
     
     static func last_month(context: NSManagedObjectContext, story_only: Bool = false) -> [Expense] {
         let request = NSFetchRequest<Expense>(entityName: "Expense")
+        request.sortDescriptors = [NSSortDescriptor(key: "spentAt_", ascending: false)]
         
         // TODO: Date
         request.predicate = NSPredicate(format: "spentAt_ > %@", Date() - 1.months as NSDate)
@@ -21,6 +23,27 @@ extension Expense {
             request.predicate = NSPredicate(format: "story != nil")
         }
         return (try? context.fetch(request)) ?? []
+    }
+    
+    static func continous_days(context: NSManagedObjectContext) -> [DateInRegion] {
+        let all_expenses = Expense.all(context: context)
+        var days = [DateInRegion]()
+        // 明天
+        var last_day = DateInRegion(region: regionChina).dateAt(.startOfDay) + 1.days
+        
+        for expense in all_expenses {
+            let now_day = expense.spentAt.dateAt(.startOfDay)
+            if now_day == last_day {
+                continue
+            } else if now_day + 1.days == last_day {
+                days.append(now_day)
+                last_day = now_day
+            } else {
+                break
+            }
+        }
+        
+        return days
     }
     
     // MARK: Update
@@ -76,5 +99,19 @@ extension Array where Element: Expense {
     
     func max_expense() -> Expense? {
         return self.max { $0.amount < $1.amount }
+    }
+    
+    func story_only() -> [Expense] {
+        return self.filter { $0.story != nil }
+    }
+    
+    func days() -> [DateInRegion] {
+        // 将 expense 的时间设为当天的起始时间
+        let raw_days = self.map { $0.spentAt.dateAt(.startOfDay) }
+        let days = raw_days.enumerated().filter { index, day in
+            index == raw_days.firstIndex(of: day)
+        }
+        
+        return days.map { $0.element }
     }
 }
