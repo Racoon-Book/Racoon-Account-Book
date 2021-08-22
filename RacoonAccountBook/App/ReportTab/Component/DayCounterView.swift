a1ced718c92846da0e49894a3a8b9399134b9dab
c0per <60544162+c0per@users.noreply.github.com>
2021-08-22 11:01:55 +0800

    fix DayCounterView

--- RacoonAccountBook/App/ReportTab/Component/DayCounterView.swift
+++ RacoonAccountBook/App/ReportTab/Component/DayCounterView.swift
@@ -2,12 +2,14 @@ import SwiftDate
 import SwiftUI
 
 struct DayCounterView: View {
-    var monthBook: Ex
+    @Environment(\.managedObjectContext) var context
     
+    var expenses: [Expense]
+
     var body: some View {
-        let days = GetDays()
-        let dayCount = GetContinuousDayCount(days)
-        let storyCount = monthBook.items.filter { $0.metadata.story != nil }.count
+        let days = expenses.days()
+        let dayCount = Expense.continous_days(context: context).count
+        let storyCount = expenses.story_only().count
         
         ZStack(alignment: .leading) {
             RoundedRectangle(cornerRadius: 15)
@@ -41,38 +43,5 @@ struct DayCounterView: View {
             }
             .padding()
         }
-    }
-    
-    // 获得不重复的记账日
-    func GetDays() -> [Int] {
-        let rawDays = monthBook.items.map { $0.metadata.spentMoneyAt.day }
-            .sorted()
-        
-        // 去重
-        var re = [Int]()
-        for (index, day) in rawDays.enumerated() {
-            if index == 0 || day != rawDays[index - 1] {
-                re.append(day)
-            }
-        }
-        
-        return re
-    }
-    
-    // 获得从今天开始的连续记账天数
-    func GetContinuousDayCount(_ rawDays: [Int]) -> Int {
-        let days = rawDays.sorted(by: >)
-        // 今天没有记账
-        if days.first != DateInRegion(region: regionChina).day {
-            return 0
-        } else {
-            for (index, day) in days.enumerated() {
-                if index > 0, day < days[index - 1] - 1 {
-                    return index
-                }
-            }
-        }
-        // 记账未中断
-        return days.count
     }
 }
