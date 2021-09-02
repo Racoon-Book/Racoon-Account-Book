63b6d4bf80291b0e57b71f872854e145b17347cb
yxj <564197835@qq.com>
2021-09-02 22:17:38 +0800

    add Expense.properties

--- RacoonAccountBook/CoreData/Extension/Expense+.swift
+++ RacoonAccountBook/CoreData/Extension/Expense+.swift
@@ -14,7 +14,7 @@ extension Expense {
     static var request_allExpensesWithStory: NSFetchRequest<Expense> {
         let request = NSFetchRequest<Expense>(entityName: "Expense")
         request.sortDescriptors = [NSSortDescriptor(key: "spentAt_", ascending: false)]
-        request.predicate = NSPredicate(format: "story != nil")
+        request.predicate = NSPredicate(format: "story_ != nil")
         return request
     }
     
@@ -27,24 +27,50 @@ extension Expense {
 
     // MARK: - access
     
-    // Expense:
+    // Expense
     //   - system: createdAt updatedAt
     //   - properties: spentAt event amount
-    //   - other: originalText
-    //   - relation: generatedTags tags focus forWho story
+    //   - other: originalText?
+    //   - relationship: generatedTags tags focus forWho story?
+    // 注：列表不会是nil 判空即可
 
+    // system
+    
     var createdAt: DateInRegion {
-        get { createdAt_!.convertTo(region: regionChina) }
+        get {
+            if createdAt_ != nil {
+                return createdAt_!.convertTo(region: regionChina)
+            } else {
+                print("未获取到createdAt_")
+                return DateInRegion(region: regionChina)
+            }
+        }
         set { createdAt_ = newValue.date }
     }
     
     var updatedAt: DateInRegion {
-        get { updatedAt_!.convertTo(region: regionChina) }
+        get {
+            if updatedAt_ != nil {
+                return updatedAt_!.convertTo(region: regionChina)
+            } else {
+                print("未获取到updatedAt_")
+                return DateInRegion(region: regionChina)
+            }
+        }
         set { updatedAt_ = newValue.date }
     }
     
+    // properties
+    
     var spentAt: DateInRegion {
-        get { spentAt_!.convertTo(region: regionChina) }
+        get {
+            if spentAt_ != nil {
+                return spentAt_!.convertTo(region: regionChina)
+            } else {
+                print("未获取到spentAt_")
+                return DateInRegion(region: regionChina)
+            }
+        }
         set { spentAt_ = newValue.date }
     }
     
@@ -59,7 +85,33 @@ extension Expense {
         }
         set { event_ = newValue }
     }
-
+    
+    var amount: Float {
+        get {
+            return amount_
+        }
+        set { amount_ = newValue }
+    }
+    
+    // other
+    
+    var originalText: String? {
+        get { originalText_ }
+        set { event_ = newValue }
+    }
+    
+    // relationship
+    
+    var story: Story? {
+        get { story_ }
+        set { story_ = newValue }
+    }
+    
+//    var flights: Set<Flight> {
+//        get { (flights_ as? Set<Flight>) ?? [] }
+//        set { flights_ = newValue as NSSet }
+//    }
+    
     // MARK: - operation
     
     static func create(expenseInfo: ExpenseInfo, context: NSManagedObjectContext) {
