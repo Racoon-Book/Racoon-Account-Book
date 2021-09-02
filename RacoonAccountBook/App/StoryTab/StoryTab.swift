a7f65fced177020f8a76b5752f71cc9130e45f54
yxj <564197835@qq.com>
2021-09-02 22:49:23 +0800

    use @FetchRequest refactor StoryTab

--- RacoonAccountBook/App/StoryTab/StoryTab.swift
+++ RacoonAccountBook/App/StoryTab/StoryTab.swift
@@ -3,16 +3,17 @@ import SwiftUI
 
 struct StoryTab: View {
     @Environment(\.managedObjectContext) var context
-  
-    var body: some View {
-        let expenses = Expense.getAllExpenses(context: context, story_only: true)
 
+    @FetchRequest(fetchRequest: Expense.request_allExpensesWithStory)
+    var enpensesWithStory
+
+    var body: some View {
         // TODO: 像账本那样做一个深色的统计页面应该比较好
-        if expenses.count != 0 {
+        if enpensesWithStory.count != 0 {
             NavigationView {
                 ScrollView(.vertical) {
                     VStack {
-                        ForEach(expenses, id: \.self) { expense in
+                        ForEach(enpensesWithStory, id: \.self) { expense in
                             ItemStoryView(expense: expense)
                         }
                         .padding(.horizontal, 10) // 让圆角矩形边框不靠边
