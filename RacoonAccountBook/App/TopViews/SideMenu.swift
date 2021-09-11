e07eba9a70e8629a15897d6ab1fb11f411b70b05
c0per <60544162+c0per@users.noreply.github.com>
2021-09-11 10:41:17 +0800

    fix only capture tap gesture when menu opens

--- RacoonAccountBook/App/TopViews/SideMenu.swift
+++ RacoonAccountBook/App/TopViews/SideMenu.swift
@@ -13,14 +13,20 @@ struct SideMenu: View {
                 .offset(x: -CGFloat(menuWidth * sideMenuOffsetX))
                 .animation(.default)
 
-            // 使Spacer()可以接受TapGesture
-            ZStack {
-                Color.black.opacity(0.001)
+            if sideMenuOffsetX == 0 {
+                // 菜单完全打开时，使用右侧Spacer接收Tap，以关闭菜单
+                ZStack {
+                    // 使Spacer()可以接受TapGesture
+                    Color.black.opacity(0.001)
+                    Spacer()
+                }
+                .onTapGesture {
+                    withAnimation { onSideMenuClose() }
+                    print(Log().debug + "点击")
+                }
+            } else {
+                // 菜单未完全打开时，不使用右侧Spacer接收Tap
                 Spacer()
-            }
-            .onTapGesture {
-                withAnimation { onSideMenuClose() }
-                print(Log().debug + "点击")
             }
         }
     }
