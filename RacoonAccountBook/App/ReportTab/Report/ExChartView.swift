b9464bea9f21d7a21b43eddde665106224133e09
c0per <60544162+c0per@users.noreply.github.com>
2021-09-11 16:53:19 +0800

    remove dependency SwiftUICharts

--- RacoonAccountBook/App/ReportTab/Report/ExChartView.swift
+++ RacoonAccountBook/App/ReportTab/Report/ExChartView.swift
@@ -1,11 +1,11 @@
 import SwiftUI
-import SwiftUICharts
+// import SwiftUICharts
 
 struct ExChartView: View {
     var expenses: [Expense]
 
     var body: some View {
         let data = expenses.map { Double($0.amount) }
-        LineChartView(data: data, title: "本月支出", form: ChartForm.large, rateValue: nil, dropShadow: false)
+        //LineChartView(data: data, title: "本月支出", form: ChartForm.large, rateValue: nil, dropShadow: false)
     }
 }
