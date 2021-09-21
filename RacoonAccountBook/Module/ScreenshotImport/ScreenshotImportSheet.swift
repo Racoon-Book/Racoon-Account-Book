79a16582cbfe5ac61333aaa8454a41cc08c72a01
yxj <564197835@qq.com>
2021-09-21 16:06:46 +0800

    add navigationBar

--- RacoonAccountBook/Module/ScreenshotImport/ScreenshotImportSheet.swift
+++ RacoonAccountBook/Module/ScreenshotImport/ScreenshotImportSheet.swift
@@ -10,7 +10,7 @@ struct ScreenshotImportSheet: View {
     @EnvironmentObject var RacoonSheetConfig: SheetConfigModel
 
     /// 在导入界面上显示的图片
-    @State private var displayImage: Image?
+    @State private var displayImage: Image? = nil
 
     /// 是否正在显示系统的照片选择器
     @State private var showingImagePicker = false
@@ -22,73 +22,128 @@ struct ScreenshotImportSheet: View {
     @State var weChatBills: [WeChatBillInfo] = []
 
     var body: some View {
-        VStack {
-            if displayImage != nil {
-                HStack {
-                    displayImage!
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 60)
-                    // TODO: 添加微信图标
+        NavigationView {
+            VStack {
+                if displayImage != nil {
+                    HStack {
+                        displayImage!
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60)
+                        // TODO: 在添加支付宝导入之后添加微信图标
 
-                    Button(action: {
-                        print("clicked 添加新账单")
-                    }) {
-                            Label("添加新账单", systemImage: "photo")
+                        // TODO: 添加多张图片的导入
+//                        Button(action: {
+//                            print("clicked 添加新账单")
+//                        }) {
+//                                Label("添加新账单", systemImage: "photo")
+//                        }
                     }
                 }
-            }
 
-            if weChatBills.count != 0 {
-                Text("识别出\(weChatBills.count)条账目")
+                if weChatBills.count != 0 {
+                    Text("识别出\(weChatBills.count)条支出")
 
-                VStack(alignment: .leading) {
-                    Text("总支出\(weChatBills.sum())")
-
-                    ForEach(Array(weChatBills.enumerated()), id: \.element) { i, weChatBill in
-                        HStack {
-                            // weChatBillView
-                            Image(systemName:
-                                weChatBill.isSelected ? "checkmark.circle" : "circle")
-                            VStack(alignment: .leading) {
+                    VStack(alignment: .leading) {
+                        Text("总支出\(weChatBills.sum())")
+                        ScrollView {
+                            ForEach(Array(weChatBills.enumerated()), id: \.element) { i, weChatBill in
                                 HStack {
-                                    Text(weChatBill.name)
-                                    Spacer()
-                                    Text("\(weChatBill.amount)") // TODO: 两位小数
+                                    // weChatBillView
+                                    Image(systemName:
+                                        weChatBill.isSelected ? "checkmark.circle" : "circle")
+                                    VStack(alignment: .leading) {
+                                        HStack {
+                                            Text(weChatBill.name)
+                                            Spacer()
+                                            Text("\(weChatBill.amount)") // TODO: 两位小数
+                                        }
+                                        Text(DisplayDate(weChatBill.spentAt))
+                                    }
                                 }
-                                Text(DisplayDate(weChatBill.spentAt))
+                                .onTapGesture {
+                                    weChatBills[i].isSelected.toggle()
+                                    print(Log().string + "tapped")
+                                }
                             }
                         }
-                        .onTapGesture {
-                            weChatBills[i].isSelected.toggle()
-                            print(Log().string + "tapped")
-                        }
                     }
-                }
 
-                LargeButton(title: "导入所选账目",
-                            backgroundColor: Color.blue,
-                            foregroundColor: Color.white) {
-                        print(Log().string + "按钮点击")
-                        // TODO: 将选中的条目添加到数据库并清除现场
-                }
-            } else {
-                // WeChatBills.count == 0
-                Text("还没有选择截图哦")
-                LargeButton(title: "选择微信账单截图",
-                            backgroundColor: Color.blue,
-                            foregroundColor: Color.white) {
-                        showingImagePicker = true
+                    LargeButton(title: "导入所选账目",
+                                backgroundColor: Color.blue,
+                                foregroundColor: Color.white) {
+                            print(Log().string + "按钮点击")
+                            // TODO: 将选中的条目添加到数据库并清除现场
+                    }
+                } else {
+                    // WeChatBills.count == 0
+                    Spacer() // FIXME: 在ScrollView中没有效果
+                    Text("还没有选择截图哦")
+                        .padding()
+                    Spacer()
+                    LargeButton(title: "选择微信账单截图",
+                                backgroundColor: Color.blue,
+                                foregroundColor: Color.white) {
+                            showingImagePicker = true
+                    }
+                    .padding()
                 }
             }
-        }
-        .sheet(isPresented: $showingImagePicker,
-               onDismiss: textExtractFromScreenshot) {
-                ImagePicker(image: self.$systemScreenshot)
+            .navigationBarTitle(
+                Text("截图导入"),
+                displayMode: .inline)
+            // [Sheet左右两侧的按钮]
+            .navigationBarItems(
+                // 左边有两个按钮
+                leading:
+                HStack {
+                    Button(action: {
+                        print(Log().string + "左上角取消点击")
+
+                        ClearImportSheetState() // 清空正在输入的 ExpenseInfo
+
+                        RacoonSheetConfig.shared.showingScreenshotImportSheet = false // 收回sheet
+                    }) { Text("取消").bold() }
+
+                    if weChatBills.count != 0 {
+                        Button(action: {
+                            print(Log().string + "左上角清除点击")
+                            withAnimation {
+                                ClearImportSheetState()
+                            }
+                        }) { Text("清除").bold() }
+                    } else {
+                        EmptyView()
+                    }
+                },
+                // 右边有一个按钮
+                trailing:
+                HStack {
+                    if weChatBills.count != 0 {
+                        Button(action: {
+                            print(Log().string + "右上角导入点击")
+
+                        }) { Text("导入").bold() }
+                    } else {
+                        EmptyView()
+                    }
+                })
+
+            .sheet(isPresented: $showingImagePicker,
+                   onDismiss: textExtractFromScreenshot) {
+                    ImagePicker(image: self.$systemScreenshot)
+            }
         }
     }
 
-    func textExtractFromScreenshot() {
+    private func ClearImportSheetState() {
+        displayImage = nil
+        showingImagePicker = false
+        systemScreenshot = nil
+        weChatBills = []
+    }
+
+    private func textExtractFromScreenshot() {
         // 清空WeChatBills
         weChatBills = []
 
@@ -129,7 +184,7 @@ struct ScreenshotImportSheet: View {
         }
     }
 
-    func recognizeTextHandler(request: VNRequest, error: Error?) {
+    private func recognizeTextHandler(request: VNRequest, error: Error?) {
         guard let observations =
             request.results as? [VNRecognizedTextObservation]
         else {
