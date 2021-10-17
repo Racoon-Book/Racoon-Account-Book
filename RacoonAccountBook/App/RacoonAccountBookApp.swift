e86f17d50794ef0aa88b15149346cb022ab42420
yxj <564197835@qq.com>
2021-10-17 15:50:39 +0800

    initially add launch screen

--- RacoonAccountBook/App/RacoonAccountBookApp.swift
+++ RacoonAccountBook/App/RacoonAccountBookApp.swift
@@ -29,7 +29,7 @@ struct RacoonAccountBookApp: App {
             }
 
             if Focus.focusAmount(context: persistenceController.container.viewContext) == 0 {
-                let focusList: [String] = ["电子设备", "软件服务", "聚餐", "游戏", "宿舍", "支持", "旅游"]
+                let focusList: [String] = ["电子设备", "软件服务", "聚餐", "游戏", "宿舍", "支持", "旅游"] // 初始化的focusList 只在用户第一次打开才会是这样
                 for focus in focusList {
                     Focus.focus(name: focus, context: persistenceController.container.viewContext)
                 }
