0e9717a0d54f36d47e9c02a26398ccd84ea8a795
yxj <564197835@qq.com>
2021-11-12 20:52:28 +0800

    fix bug: FocusPieChart breaks when no focus exists

--- RacoonAccountBook/App/ReportTab/Focus/FocusChartView.swift
+++ RacoonAccountBook/App/ReportTab/Focus/FocusChartView.swift
@@ -11,12 +11,18 @@ struct FocusChartView: View {
 
     var body: some View {
         VStack {
-            PieChartView(
-                values: sumList,
-                names: focusNameList,
-                formatter: { value in String(format: "%.1f", value) },
-                backgroundColor: defaultColorSet.tabBackground
-            )
+            if sumList.reduce(0.0) { $0 + $1 } == 0 {
+                // 这里要统计的是所有有Focus的支出的钱数 不为0就可以绘图
+                Text("使用「关注」来管理财记 重点分明")
+                Text("添加一笔含有「关注」的花销吧！")
+            } else {
+                PieChartView(
+                    values: sumList,
+                    names: focusNameList,
+                    formatter: { value in String(format: "%.1f", value) },
+                    backgroundColor: defaultColorSet.tabBackground
+                )
+            }
         }
         .onAppear {
             var tempFocusNameList: [String] = []
